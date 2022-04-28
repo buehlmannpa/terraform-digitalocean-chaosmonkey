c5668f373e324681cabc3784909c9406eca3d768
buehlmannpa <patrick.buehlmann.1@post.ch>
2022-04-28 12:56:06 +0200

    Splited the two terraform pipelines kubernetes and droplet

--- project.tf
+++ project.tf
@@ -1,10 +1,8 @@
-#resource "digitalocean_project" "chaosmonkey_dev" {
-#  name        = "ChaosMonkey-Development"
-#  description = "All resources for the thesis about the Kubernetes ChaosMonkey"
-#  environment = "Development"
-#
-#  resources = [
-#    digitalocean_droplet.cm_nonprod.urn,
-#    digitalocean_kubernetes_cluster.k8s_cluster_c1.urn
-#  ]
-#}
+resource "digitalocean_project" "chaosmonkey_dev" {
+  name        = "ChaosMonkey-Dev"
+  description = "All resources for the thesis about the Kubernetes ChaosMonkey"
+  environment = "Development"
+  resources = [
+    digitalocean_droplet.cm_nonprod.urn
+  ]
+}
--- variables.tf
+++ variables.tf
@@ -1,18 +1,33 @@
-variable "clustername_cluster1" {
-  type        = string
-  default     = "k8s-cluter-c1-fra1-1227"
-  description = "Name of the kubernetes cluster"
-}
-
-variable "clustername_cluster2" {
-  type        = string
-  default     = "k8s-cluter-c2-fra1-1227"
-  description = "Name of the kubernetes cluster"
-}
-
+#----------------
+# Environment Vars
+#----------------
 variable "do_token" {
   type        = string
   sensitive   = true
-  description = "API Token to access digitalocean cloud"
-  default     = "d332320576bd308e4ce59e3cd35c4a600bd2abdd94689385cbdec5d419436479"
+  description = "API Token to access digitalocean cloud - exported to the env vars of host"
+}
+
+variable "pvt_key" {
+  type        = string
+  #default     = "/Users/pbu/.ssh/id_rsa"
+  default     = "/home/buehlmannpa/.ssh/id_rsa"
+  description = "Default location of ssh key on private macbook"
+}
+variable "pub_key" {
+  type        = string
+  #default     = "/Users/pbu/.ssh/id_rsa.pub"
+  default     = "/home/buehlmannpa/.ssh/id_rsa.pub"
+  description = "Default location of ssh key on private macbook"
+}
+
+variable "region" {
+  type        = string
+  default     = "fra1"
+  description = "VPC regiion where the componentes where deployed"
+}
+
+variable "hostname" {
+  type        = string
+  default     = "vlscmn-fra1"
+  description = "Name of the kubernetes cluster"
 }
