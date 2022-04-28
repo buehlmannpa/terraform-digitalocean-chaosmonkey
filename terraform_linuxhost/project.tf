4f595df19dd4f43bd00d8530520596d3d41fad75
buehlmannpa <patrick.buehlmann.1@post.ch>
2022-04-28 13:12:04 +0200

    Splited the two terraform pipelines kubernetes and droplet

--- terraform_linuxhost/project.tf
+++
@@ -1,8 +0,0 @@
-//resource "digitalocean_project" "chaosmonkey_dev" {
-//  name        = "ChaosMonkey-Development"
-//  description = "All resources for the thesis about the Kubernetes ChaosMonkey"
-//  environment = "Development"
-//  resources = [
-//    digitalocean_droplet.cm_nonprod.urn
-//  ]
-//}
