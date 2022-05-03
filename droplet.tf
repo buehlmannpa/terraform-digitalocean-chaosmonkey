86092b456a9acc0fea9ea4c4c693549a1a0e39b0
buehlmannpa <patrick.buehlmann.1@post.ch>
2022-05-03 16:26:27 +0200

    Added a webpage to present the logs

--- droplet.tf
+++ droplet.tf
@@ -1,14 +1,28 @@
+# Create NonProd Volume for Chaos-Monkey Droplet
+resource "digitalocean_volume" "cm_nonprod" {
+  region                  = var.region
+  name                    = var.volumename
+  size                    = var.volume_size
+  initial_filesystem_type = var.volume_filesystem_type
+}
+
 # Create NonProd Chaos-Monkey Droplet
 resource "digitalocean_droplet" "cm_nonprod" {
-  image     = "ubuntu-20-04-x64"
+  image     = var.droplet_image
   name      = var.hostname
   region    = var.region
-  size      = "s-1vcpu-1gb"
+  size      = var.droplet_size
   user_data = file("cloudinit/user-data")
 
   provisioner "local-exec" {
     command = "sleep 20 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u chaosmonkey -i '${self.ipv4_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' ansible/configuration.yaml"
   }
+}
+
+# Attach Volume to Droplet
+resource "digitalocean_volume_attachment" "cm_nonprod" {
+  droplet_id = digitalocean_droplet.cm_nonprod.id
+  volume_id  = digitalocean_volume.cm_nonprod.id
 }
 
 output "hostname" {
--- variables.tf
+++ variables.tf
@@ -27,5 +27,35 @@ variable "region" {
 variable "hostname" {
   type        = string
   default     = "vlscmn-fra1"
-  description = "Name of the kubernetes cluster"
+  description = "Name of the chaos-monkey host"
+}
+
+variable "droplet_image" {
+  type        = string
+  default     = "ubuntu-20-04-x64"
+  description = "Droplet image of the chaos-monkey host"
+}
+
+variable "droplet_size" {
+  type        = string
+  default     = "s-1vcpu-1gb"
+  description = "Droptlet size of the chaos-monkey host"
+}
+
+variable "volumename" {
+  type        = string
+  default     = "vlscmn-fra1-vol1"
+  description = "Volume name of the chaos-monkey host volume"
+}
+
+variable "volume_size" {
+  type        = number
+  default     = 5
+  description = "Volume size of the chaos-monkey host volume"
+}
+
+variable "volume_filesystem_type" {
+  type        = string
+  default     = "ext4"
+  description = "Filesystem type of the chaos-monkey host volume"
 }
